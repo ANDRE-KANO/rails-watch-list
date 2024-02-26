fed468346e0fc00a6010589e3fbfb5689d80d671
andre kano <andrekano@outlook.com>
2024-02-26 13:46:21 -0300

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :bookmarks do
+    resources :list_id, only: [:show]
+    resources :movie_id, only: [:show]
+  end
+end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
